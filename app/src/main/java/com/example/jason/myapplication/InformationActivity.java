8e87591a5d1abac5dafd35e9e547a2c1ca11bd10
r05921104 <r05921104@ntu.edu.tw>
2017-03-22 17:14:54 +0800

    add image

--- app/src/main/java/com/example/jason/myapplication/InformationActivity.java
+++ app/src/main/java/com/example/jason/myapplication/InformationActivity.java
@@ -1,8 +1,14 @@
 package com.example.jason.myapplication;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 
 /**
@@ -18,14 +24,31 @@ public class InformationActivity extends Activity {
         TextView textView2 = (TextView) findViewById(R.id.textView2);
         TextView textView3 = (TextView) findViewById(R.id.textView3);
         TextView textView4 = (TextView) findViewById(R.id.textView4);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
         Bundle bundle =this.getIntent().getExtras();
         textView.setText("名稱： " + bundle.get("name"));
         textView2.setText("緯度： " + bundle.get("latitude"));
         textView3.setText("經度： " + bundle.get("longitude"));
         textView4.setText("介紹： " + bundle.get("information"));
 
+        String name = (String)bundle.get("name");
+        switch (name){
+            case "台大體育館":
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.gym));
+                break;
+            case "中正紀念堂":
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.memorialhall));
+                break;
+            case "台北車站":
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.station));
+                break;
+        }
+
+
+
 
 
 
     }
+
 }
--- app/src/main/java/com/example/jason/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/jason/myapplication/MapsActivity.java
@@ -61,10 +61,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng tauyuan = new LatLng(25.04, 121.33);
         mMap.setPadding(20, 20, 20, 20);
 
+        //get the initial position
+        getInitialPosition(mMap);
+
+        //deposit the information
+        db = openOrCreateDatabase("db1.db", MODE_PRIVATE,null);
+        depositInformation(db);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -76,51 +80,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        LocationManager locationMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        double latitude = 25;
-        double longitude = 121;
-        LatLng newLatLng = new LatLng(latitude, longitude);
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(newLatLng)              // Sets the center of the map to ZINTUN
-                .zoom(20)                   // Sets the zoom
-                .bearing(90)                // Sets the orientation of the camera to east
-                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                .build();                   // Creates a CameraPosition from the builder
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        mMap.addMarker(new MarkerOptions().position(newLatLng).title("Marker"));
-
-
         int minTime = 1000;//ms
         int minDist = 1;//meter
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-        db = openOrCreateDatabase("db1.db", MODE_PRIVATE,null);
-        try{
-            db.execSQL("DROP TABLE table01");
-            String createTable01 = "CREATE TABLE table01(_id INTEGER PRIMARY KEY , num INTEGER , data TEXT, latitude NUMERIC, longitude NUMERIC, information TEXT)";
-            db.execSQL(createTable01);
-            String insert_1 = "INSERT INTO table01 (num,data,latitude,longitude,information) values (1, '國立故宮博物院', 25.103016, 121.548449, '國立故宮博物院（簡稱故宮或故宮博物院，俗稱臺灣故宮或臺北故宮，別名中山博物院），坐落在中華民國臺北市士林區至善路2段221號和嘉義縣太保市故宮大道888號，為臺灣規模最大的博物館以及八景之一，同時也是古代中國藝術史與漢學研究重鎮，所藏近70萬件冊文物尤以古代中國藝術珍藏著稱。館舍一年可接待超過614萬人次參訪旅客，位居全球參觀人數最多的藝術博物館前列。')";
-            db.execSQL(insert_1);
-            String insert_2 = "INSERT INTO table01 (num,data,latitude,longitude,information) values (2, '台大體育館', 25.021918, 121.535285,'國立臺灣大學綜合體育館（NTU Sports Center，或稱台大巨蛋體育館、台大小巨蛋[1]）是國立臺灣大學總校區的一項設施，座落於臺北市大安區辛亥路與新生南路交叉口，無獨立門牌號碼，台大校內一般稱呼為新體。建設目的是為了替代台大內的舊體育館以提供更多的運動休閒空間與更新的運動器材，解決台大校內缺乏可以舉辦大型活動的場所的問題；並因為該館符合其他非台大校內團體的需求，亦外借給其他社會團體舉辦活動。')";
-            db.execSQL(insert_2);
-            String insert_3 = "INSERT INTO table01 (num,data,latitude,longitude,information) values (3, '台北車站', 25.047908, 121.517315, '')";
-            db.execSQL(insert_3);
-
-
-
-        }
-        catch (Exception exception){
-        }
-
+        LocationManager locationMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDist, (android.location.LocationListener) locationListener);
 
     }
@@ -157,7 +119,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     double latitude_i = query.getDouble(2);
                     double longitude_i = query.getDouble(3);
                     double distance = Distance(longitude_i, latitude_i, currentLongitude, currentLatitude);
+                    Log.e("distance", distance+"");
                     if(distance > shortestDis) {
+                        query.moveToNext();
                         continue;
                     }
                     else{
@@ -181,22 +145,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
 
             }
-
-            /*double gymLongitude = 121.535285;
-            double gymLatitude = 25.021918;
-            double gymDistance = Distance(currentLongitude, currentLatitude, gymLongitude, gymLatitude);
-
-            double stationLongitude = 121.517315;
-            double stationLatitude = 25.047908;
-            double stationDistance = Distance(currentLongitude, currentLatitude, stationLongitude, stationLatitude);
-
-            Log.e("distance",Double.toString(gymDistance));
-            Log.e("distance",Double.toString(stationDistance));
-            MarkerOptions gymMarkerOption = new MarkerOptions().position(new LatLng(gymLatitude, gymLongitude)).title("gym");
-            final Marker gymMarker = mMap.addMarker(gymMarkerOption);
-            MarkerOptions stationMarkerOption = new MarkerOptions().position(new LatLng(stationLatitude, stationLongitude)).title("station");
-            Marker stationMarker = mMap.addMarker(stationMarkerOption);*/
-
             MarkerOptions shortestmarkerOption = new MarkerOptions().position(new LatLng(latitude, longitude)).title(name);
             final Marker shortestmarker = mMap.addMarker(shortestmarkerOption);
             mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
@@ -213,13 +161,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     return false;
                 }
             });
-
-
-
-
-
-
-
         }
 
         @Override
@@ -251,6 +192,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         distance = Math.round(distance * 10000) / 10000;
 
         return distance ;
+    }
+
+    public void getInitialPosition(GoogleMap mMap){
+        double latitude = 25;
+        double longitude = 121;
+        LatLng newLatLng = new LatLng(latitude, longitude);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(newLatLng)              // Sets the center of the map to ZINTUN
+                .zoom(20)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().position(newLatLng).title("Marker"));
+    }
+    public void depositInformation(SQLiteDatabase db){
+        try{
+            db.execSQL("DROP TABLE table01");
+            String createTable01 = "CREATE TABLE table01(_id INTEGER PRIMARY KEY , num INTEGER , data TEXT, latitude NUMERIC, longitude NUMERIC, information TEXT)";
+            db.execSQL(createTable01);
+
+            String insert_1 = "INSERT INTO table01 (num,data,latitude,longitude,information) values (1, '中正紀念堂', 25.035556, 121.519722, '中正紀念堂是一座為紀念故前中華民國總統蔣中正而興建的建築，位於臺北市中正區，也是眾多紀念蔣中正總統的建築中規模最大者。全區250,000平方公尺，主樓高76公尺，管理機關為「國立中正紀念堂管理處」；而園區廣場的南北側，另建有國家表演藝術中心國家兩廳院管理的國家戲劇院和國家音樂廳，落成以來成為臺北市在國際上最著名地標之一。')";
+            db.execSQL(insert_1);
+            String insert_2 = "INSERT INTO table01 (num,data,latitude,longitude,information) values (2, '台大體育館', 25.021918, 121.535285,'國立臺灣大學綜合體育館（NTU Sports Center，或稱台大巨蛋體育館、台大小巨蛋[1]）是國立臺灣大學總校區的一項設施，座落於臺北市大安區辛亥路與新生南路交叉口，無獨立門牌號碼，台大校內一般稱呼為新體。建設目的是為了替代台大內的舊體育館以提供更多的運動休閒空間與更新的運動器材，解決台大校內缺乏可以舉辦大型活動的場所的問題；並因為該館符合其他非台大校內團體的需求，亦外借給其他社會團體舉辦活動。')";
+            db.execSQL(insert_2);
+            String insert_3 = "INSERT INTO table01 (num,data,latitude,longitude,information) values (3, '台北車站', 25.047908, 121.517315, '臺北車站於1891年7月5日設站，歷經多次遷移與改建後，現今站體為1989年9月2日啟用，臺鐵局本部也設於此地；隨著捷運、高鐵進駐之後，形成幅員廣闊的地下街區。車站周邊有商辦、補習班聚集的站前商圈，若再連結臺北轉運站、捷運北門站、機場捷運臺北站、華山特區等「臺北車站特定區」關聯設施，面積可達46.31公頃[8]。另外臺北車站常被稱為「臺北火車站」，臺語則稱為「臺北火車頭」或「臺北車頭」，年輕族群則常簡稱為「北車」')";
+            db.execSQL(insert_3);
+        }
+        catch (Exception exception){
+        }
     }
 
 }
